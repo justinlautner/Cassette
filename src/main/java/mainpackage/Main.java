33f4c7b0858ad28ff3158c75efdb4368a1e633ba
justinlautner <justinjlautner@gmail.com>
2020-10-21 15:38:53 -0500

    Add program icon

--- src/main/java/mainpackage/Main.java
+++ src/main/java/mainpackage/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.File;
 import java.net.URL;
@@ -22,6 +23,7 @@ public class Main extends Application {
         controller.setStage(primaryStage);
         controller.loadMusic();
         primaryStage.setTitle("Cassette");
+        primaryStage.getIcons().add(new Image("icons/cassette-icon.png"));
         primaryStage.setScene(new Scene(root, 1425, 775));
 
         primaryStage.show();
